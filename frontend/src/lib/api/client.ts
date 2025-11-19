01a6185d09bb6416b8dce6b55b2cf30350ef77b8
twkim <angelos0424@gmail.com>
2025-11-19 21:13:19 +0900

    listPost api 수정

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,12 +1,25 @@
 import { fetchApi } from '../queryClient';
-import {Account, AccountsResponse, FeedQuery, FeedResponse, PostResponse, PostsRequest, PostsResponse} from './types';
+import {AccountsResponse, PostResponse, PostsRequest, PostsResponse} from './types';
 
 export const CLIENT_KEY = 'client';
 
 export const listAccount = () => fetchApi.get<AccountsResponse>('/accounts').then(res => res.data);
 
 
-export const listPost = (req: PostsRequest) => fetchApi.get<PostsResponse>('/posts', req).then(res => res.data);
+export const listPost = async (params: PostsRequest) => {
+  const search = new URLSearchParams();
+  if (params.accountId) search.set('accountId', params.accountId);
+  if (params.from) search.set('from', params.from);
+  if (params.to) search.set('to', params.to);
+  if (params.pageSize) search.set('limit', String(params.pageSize));
+  if (params.page) search.set('page', String(params.page));
+  if (params.cursor) search.set('cursor', params.cursor);
+
+  const qs = search.toString() ? '?' + search.toString() : '';
+
+  const res = await fetchApi.get<PostsResponse>(`/posts${qs}`);
+  return res.data;
+}
 
 
 export const detailPost = (postId: string) => fetchApi.get<PostResponse>(`/posts/${postId}`);
