c73de3995834256319b25f59d30a14d0d4362b1f
twkim <angelos0424@gmail.com>
2025-11-28 15:27:25 +0900

    수정

--- backend/src/verify_sig.ts
+++ backend/src/verify_sig.ts
@@ -1,9 +1,9 @@
-const crypto = require('crypto');
+import crypto from 'crypto';
 
 const secret = 'a7d8e9f2b3c4d5e6f7a8b9c0d1e2f3a4';
 const path = 'fit-in/640x640/filters:format(webp):quality(80)/jiheonnibaek/2025-11-26_15-39-45_UTC_3.jpg';
 
-function sign(p) {
+function sign(p: string) {
     const hmac = crypto.createHmac('sha1', secret);
     hmac.update(p);
     return hmac.digest('base64url');
